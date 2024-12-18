453e58e607da794d70476222af39347c3d930562
Joe Sanders <kernel528@gmail.com>
2024-12-18 15:11:24 -0600

    Updated for module 3.10.9 with map-properties.js and lodash node_module.  Updated products.service.js with updated read function with join and mapProperties require.

--- src/products/products.service.js
+++ src/products/products.service.js
@@ -1,11 +1,26 @@
 const knex = require("../db/connection");
+// Module 3.10.9
+const mapProperties = require("../utils/map-properties");
+
+const addCategory = mapProperties({
+    category_id: "category.category_id",
+    category_name: "category.category_name",
+    category_description: "category.category_description",
+});
 
 function list() {
     return knex("products").select("*");
 }
 
-function read(productId) {
-    return knex("products").select("*").where({ product_id: productId }).first();
+// Module 3.10.9
+function read(product_id) {
+    return knex("products as p")
+        .join("products_categories as pc", "p.product_id", "pc.product_id")
+        .join("categories as c", "pc.category_id", "c.category_id")
+        .select("p.*", "c.*")
+        .where({ "p.product_id": product_id })
+        .first()
+        .then(addCategory);
 }
 
 // List Out of Stock products...
